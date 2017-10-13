641a7f7a3eab75484c0f4be5e7b659ffd1c9fc82
marilyn52 <m-mags@hotmail.co.uk>
2017-10-13 16:57:54 +0100

    friday

---
+++ resume/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Resume(models.Model):
+    owner = models.ForeignKey('auth.User')
+    name = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    last_updated = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
---
+++ resume/views.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.utils import timezone
+from .models import Resume
+from django.shortcuts import render, get_object_or_404
+from .forms import ResumeForm
+
+# Create your views here.
+def resume_list(request):
+    resumes = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'jobpost/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'jobpost/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('/user/dashboard', pk=post.pk)
+
+    else:
+        form = PostForm()
+    return render(request, 'jobpost/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'jobpost/post_edit.html', {'form': form})
