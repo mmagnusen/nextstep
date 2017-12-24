e610c66c7c0f71db5674a3cc6ea32286478de470
mmagnusen <m-mags@hotmail.co.uk>
2017-12-24 01:59:43 +0000

    before deploy

--- nextstep/settings.py
+++ nextstep/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'iuv-e*qq7llgobjoy%1f%aj)7q*r*5wda(=atyjb_4zvq$z#i3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
