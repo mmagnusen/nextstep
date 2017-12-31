05aa0df4a4eb7af7f3ea4449e0a0be9906040e5c
mmagnusen <m-mags@hotmail.co.uk>
2017-12-31 20:31:17 +0000

    Added pythonanywhere settings and localsettings

---
+++ nextstep/pythonanywhere_settings.py
@@ -0,0 +1,12 @@
+from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nextstep',
+        'USER': 'nextstepuser',
+        'PASSWORD': 'Zimbardo123@',
+        'HOST': 'marilynmags-625.postgres.pythonanywhere-services.com',
+        'PORT': '10625',
+    }
+}
--- nextstep/settings.py
+++ nextstep/settings.py
@@ -87,12 +87,7 @@ LOGOUT_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
